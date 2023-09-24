5bce8b536214614015462c355bd1194df34c853e
Alen <info@alenbarac.com>
2023-09-24 18:08:33 +0200

    inital data fetch using useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import Header from './Header'
 import Body from './Body'
+import { useEffect, useReducer } from 'react'
 
 function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'dataRecived':
+        return {
+          ...state,
+          questions: action.payload,
+          status: 'ready',
+        }
+      case 'dataFailed':
+        return {
+          ...state,
+          status: 'error',
+        }
+
+      default:
+        throw new Error('Action unknown')
+    }
+  }
+
+  const initialState = {
+    questions: [],
+    // loading,error,ready,active,finished
+    status: 'loading',
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataRecived', payload: data }))
+      .catch((error) => dispatch({ type: 'dataFailed,' }))
+  }, [])
+
   return (
     <div className="app">
       <Header />
